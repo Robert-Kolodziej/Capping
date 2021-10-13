f064f33d1aa89e68b7f5d856871a4381610ae27a
Robert-Kolodziej <robert.kolodziej1@marist.edu>
2021-10-13 10:28:30 -0400

    Update DemoBlockcian.py

--- DemoBlockcian.py
+++ DemoBlockcian.py
@@ -1,7 +1,10 @@
+import json
 from hashlib import sha256
+
+
 class Block:
     def __init__(self, index, certificate, previous_hash, nonce=0):
-        self.index =index
+        self.index = index
         self.certificate = certificate
         self.previous_hash = previous_hash
         self.nonce = nonce
@@ -9,6 +12,7 @@ class Block:
     def compute_hash(self):
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
+
 
 class Blockchain:
 
@@ -22,15 +26,18 @@ class Blockchain:
         genesis_block = input
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
+        print(genesis_block)
 
     def create_second_block(self):
         input("Input the school where the degree was achieved:  ")
         second_block = input
         second_block.hash = second_block.compute_hash()
         self.chain.append(second_block)
+        print(second_block)
 
     def create_third_block(self):
         input("Input the year the degree was achieved:  ")
         third_block = input
         third_block.hash = third_block.compute.hash()
         self.chain.append(third_block)
+        print(third_block)
