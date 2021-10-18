617a1f6542700736943d942c2aa1ca066c6691fd
Robert-Kolodziej <robert.kolodziej1@marist.edu>
2021-10-18 15:05:36 -0400

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('Demo Blockchain site.html')
 
+
 @app.route('/post', methods=['POST'])
 def post():
-    return "recived: {}".format(request.form)
+    return "received: {}".format(request.form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
