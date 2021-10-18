93162eb45296ec2c3738ffd56dab4aae64486116
Robert-Kolodziej <robert.kolodziej1@marist.edu>
2021-10-18 15:02:58 -0400

    Update Flask.py

--- templates/Flask.py
+++ templates/Flask.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('Demo Blockchain site.html')
 
 @app.route('/post', methods=['POST'])
 def post():
