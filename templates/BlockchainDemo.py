4bf2a498aa0ceb0a62d36287722024547dc92c60
Robert-Kolodziej <robert.kolodziej1@marist.edu>
2021-10-26 17:54:03 -0400

    Update BlockchainDemo.py
    attempting to hardcode for demo

--- templates/BlockchainDemo.py
+++ templates/BlockchainDemo.py
@@ -4,21 +4,17 @@ import random
 
 import self as self
 
-input("What do you want to hash?: ")
-nonce = random.randint
+nonce = 1
 previous_hash = 0
 block = 1
-self.chain = []
+chain = []
+create_block = []
 class Blockchain:
-
-    def __init__(self):
-        self.chain = []
-        self.create_block(input, nonce, previous_hash)
-
-    def create_block(self, input, nonce, previous_hash):
-        block = {'input': len(self.chain) + 1, 'nonce': nonce, 'previous_hash': previous_hash}
-        self.chain.append(block)
-        return block
+    input("What do you want to hash?: ")
+    def create_block(input, nonce, previous_hash):
+        block = (input, nonce, previous_hash)
+        chain.append(block)
+        print(block)
 
     def get_previous_block(input):
         return input.chain[-1]
